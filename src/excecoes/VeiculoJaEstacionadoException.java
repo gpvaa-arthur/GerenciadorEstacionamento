6e72276abcfd18c4f1e20ab92c43ae3368cd3aa2
DahareWL <walacesantos620@gmail.com>
2026-01-17 12:58:22 -0300

    Add files via upload

---
+++ src/excecoes/VeiculoJaEstacionadoException.java
@@ -0,0 +1,7 @@
+package excecoes;
+
+public class VeiculoJaEstacionadoException extends RuntimeException {
+    public VeiculoJaEstacionadoException(String message) {
+        super(message);
+    }
+}
